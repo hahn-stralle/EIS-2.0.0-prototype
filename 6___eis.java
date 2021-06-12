8ffdeafd005d3087f3a3ed31b50c32224cbbf005
Hahn Stralle <84215792+hahn-stralle@users.noreply.github.com>
2021-06-13 00:16:26 +0900

    Add files via upload

--- 6___eis.java
+++ 6___eis.java
@@ -6,15 +6,17 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 
-public class eis
+public class eis extends Thread
 {
 	//手動で二つ、文字列を入力して鍵（パスワード）を生成。
 	public static String configManualKeyFront = "jurtsfdeASAw3975449eiefUQasae";
@@ -26,12 +28,12 @@ public class eis
 	public static final String CONFIG_TIME = "SSSssmmHHddMMyyyy";
 	
 	public static final String CONFIG_EXTENSION = ".eis";
-	public static final String CONFIG_VERSION = "3.2.1";
+	public static final String CONFIG_VERSION = "3.3.0";
 	public static final String CONFIG_AUTO_KEY_FOLDER_NAME = "0___holder\\";
 	public static final String CONFIG_AUTO_KEY_FILE_EXTENSION = ".autokey";
 	public static final int CONFIG_AUTO_KEY_SIZE = 256;
 	public static final int CONFIG_RANDOM_WIDTH = 512;
-	public static final int CONFIG_BUFFER_SIZE = 8192;
+	public static final int CONFIG_BUFFER_SIZE = 131072;
 	
 	public static String configAutoKeyFolderPath = "\\";
 	public static String configImportFilePath = "c:\\sample.txt";
@@ -43,6 +45,68 @@ public class eis
 	public static int configProgressSector = -1;
 	public static int configProgressSectorCounter = 0;
 	public static int configProgressPercent = 0;
+	
+	public static int configHashLength = -1;
+	
+	//マルチスレッド用変数。
+	private boolean eis;
+	private byte[][] raf;
+	private byte[] hashFront;
+	private byte[] hashRear;
+	private int hashLength;
+	private byte[] buffer;
+	
+	//マルチスレッド初期設定。
+	public eis(final boolean EIS, final byte[][] RAF, final byte[] HASH_FRONT, final byte[] HASH_REAR, final int HASH_LENGTH, final byte[] BUFFER) throws Exception
+	{
+		this.eis = EIS;
+		this.raf = RAF;
+		this.hashFront = HASH_FRONT;
+		this.hashRear = HASH_REAR;
+		this.hashLength = HASH_LENGTH;
+		this.buffer = BUFFER;
+	}
+	
+	@Override //マルチスレッド処理。
+	public void run()
+	{
+		int hashCounter = 0;
+		
+		try
+		{
+			hashCounter = this.hashLength;
+			for(int lap = 0; lap < this.buffer.length; lap++)
+			{
+				if(hashCounter == this.hashLength)
+				{
+					this.hashFront = getHash(this.hashFront);
+					this.hashRear = getHash(this.hashRear);
+					
+					hashCounter = 0;
+				}
+				
+				if(!this.eis)
+				{
+					this.buffer[lap] -= raf[(this.hashFront[hashCounter] + 128)][(this.hashRear[hashCounter] + 128)];
+				}
+				else
+				{
+					this.buffer[lap] += raf[(this.hashFront[hashCounter] + 128)][(this.hashRear[hashCounter] + 128)];
+				}
+				
+				hashCounter++;
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	public synchronized byte[] getBuffer()
+	{
+		return this.buffer;
+	}
 	
 	public static void main(String[] args) throws Exception
 	{
@@ -120,8 +184,8 @@ public class eis
 	public static byte[] getArray(final byte[] FRONT, final byte[] REAR) throws Exception
 	{
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		baos.write(FRONT);
-		baos.write(REAR);
+		if(FRONT != null)baos.write(FRONT);
+		if(REAR != null)baos.write(REAR);
 		
 		return baos.toByteArray();
 	}
@@ -196,6 +260,7 @@ public class eis
 		BufferedInputStream bis = new BufferedInputStream(fis);
 		if(fi.getPath().contains(EXPORT_DIRECTORY_PATH + fi.getName()))System.exit(0);
 		configImportFileFullSize = fi.length();
+		configHashLength = getHash(getSeed(1)).length;
 		
 		final byte[] TIME = getTime();
 		byte[] configManualKeyFrontHash = getArray(TIME, getByte(configManualKeyFront));
@@ -213,30 +278,35 @@ public class eis
 		bos.write(getByte(getFileExtension(fi.getName())));
 		
 		int bufferLimit = -1;
-		int hashCounter = getHash(getSeed(1)).length;
-		int hashLength = getHash(getSeed(1)).length;
 		byte[] buffer = new byte[CONFIG_BUFFER_SIZE];
-		byte[] cell = new byte[1];
-		RandomAccessFile[] raf = new RandomAccessFile[CONFIG_AUTO_KEY_SIZE];
-		for(int lap = 0; lap < CONFIG_AUTO_KEY_SIZE; lap++)raf[lap] = new RandomAccessFile(configAutoKeyFolderPath + CONFIG_AUTO_KEY_FOLDER_NAME + lap + CONFIG_AUTO_KEY_FILE_EXTENSION, "r");
+		/*raf*/byte[][] raf = new byte[CONFIG_AUTO_KEY_SIZE][];
+		/*raf*/for(int lap = 0; lap < raf.length; lap++)raf[lap] = Files.readAllBytes(Paths.get(configAutoKeyFolderPath + CONFIG_AUTO_KEY_FOLDER_NAME + lap + CONFIG_AUTO_KEY_FILE_EXTENSION));
 		System.out.println(fi.getPath() + "を暗号化中。しばらくお待ち下さい……。");
 		while((bufferLimit = bis.read(buffer)) != -1)
 		{
-			for(int lap = 0; lap < bufferLimit; lap++)
+			int sectorFront = 0;
+			int sectorRear = 0;
+			eis[] eisThreadCore = new eis[16];
+			for(int lap = 0; lap < eisThreadCore.length; lap++)
 			{
-				if(hashCounter == hashLength)
+				sectorRear += (CONFIG_BUFFER_SIZE / eisThreadCore.length);
+				eisThreadCore[lap] = new eis(false, raf, configManualKeyFrontHash, configManualKeyRearHash, configHashLength, Arrays.copyOfRange(buffer, sectorFront, sectorRear));
+				eisThreadCore[lap].start();
+				sectorFront += (CONFIG_BUFFER_SIZE / eisThreadCore.length);
+				
+				for(int blockChain = 0; blockChain < (CONFIG_BUFFER_SIZE / eisThreadCore.length) / configHashLength; blockChain++)
 				{
 					configManualKeyFrontHash = getHash(configManualKeyFrontHash);
 					configManualKeyRearHash = getHash(configManualKeyRearHash);
-					hashCounter = 0;
 				}
-				
-				
-				raf[(configManualKeyFrontHash[hashCounter] + 128)].seek((configManualKeyRearHash[hashCounter] + 128));
-				raf[(configManualKeyFrontHash[hashCounter] + 128)].read(cell);
-				
-				buffer[lap] -= cell[0];
-				hashCounter++;
+			}
+			
+			buffer = null;
+			for(int lap = 0; lap < eisThreadCore.length; lap++)
+			{
+				eisThreadCore[lap].join();
+				buffer = getArray(buffer, eisThreadCore[lap].getBuffer());
+				eisThreadCore[lap] = null;
 			}
 			
 			bos.write(buffer, 0, bufferLimit);
@@ -244,7 +314,6 @@ public class eis
 			displayProgress();
 		}
 		
-		for(int lap = 0; lap < CONFIG_AUTO_KEY_SIZE; lap++)raf[lap].close();
 		bis.close();
 		bos.close();
 		System.out.print("\r");
@@ -259,16 +328,18 @@ public class eis
 		FileInputStream fis = new FileInputStream(fi);
 		BufferedInputStream bis = new BufferedInputStream(fis);
 		if(!getFileExtension(fi.getName()).contains(CONFIG_EXTENSION))System.exit(0);
-		configImportFileFullSize = fi.length();
+		configHashLength = getHash(getSeed(1)).length;
 		
+		int throughLength = 0;
 		final byte[] THROUGH = new byte[(getByte(CONFIG_EXTENSION).length + getByte(CONFIG_VERSION).length)];
-		bis.read(THROUGH);
+		bis.read(THROUGH);throughLength += THROUGH.length;
 		final byte[] TIME = new byte[getTime().length];
-		bis.read(TIME);
+		bis.read(TIME);throughLength += TIME.length;
 		final byte[] EXPORT_FILE_EXTENSION_LENGTH = new byte[1];
-		bis.read(EXPORT_FILE_EXTENSION_LENGTH);
+		bis.read(EXPORT_FILE_EXTENSION_LENGTH);throughLength += EXPORT_FILE_EXTENSION_LENGTH.length;
 		final byte[] EXPORT_FILE_EXTENSION = new byte[(EXPORT_FILE_EXTENSION_LENGTH[0] * getByte("b").length)];
-		bis.read(EXPORT_FILE_EXTENSION);
+		bis.read(EXPORT_FILE_EXTENSION);throughLength += EXPORT_FILE_EXTENSION.length;
+		configImportFileFullSize = (fi.length() - throughLength);
 		
 		byte[] configManualKeyFrontHash = getArray(TIME, getByte(configManualKeyFront));
 		byte[] configManualKeyRearHash = getArray(TIME, getByte(configManualKeyRear));
@@ -279,30 +350,35 @@ public class eis
 		BufferedOutputStream bos = new BufferedOutputStream(fos);
 		
 		int bufferLimit = -1;
-		int hashCounter = getHash(getSeed(1)).length;
-		int hashLength = getHash(getSeed(1)).length;
 		byte[] buffer = new byte[CONFIG_BUFFER_SIZE];
-		byte[] cell = new byte[1];
-		RandomAccessFile[] raf = new RandomAccessFile[CONFIG_AUTO_KEY_SIZE];
-		for(int lap = 0; lap < CONFIG_AUTO_KEY_SIZE; lap++)raf[lap] = new RandomAccessFile(configAutoKeyFolderPath + CONFIG_AUTO_KEY_FOLDER_NAME + lap + CONFIG_AUTO_KEY_FILE_EXTENSION, "r");
+		/*raf*/byte[][] raf = new byte[CONFIG_AUTO_KEY_SIZE][];
+		/*raf*/for(int lap = 0; lap < raf.length; lap++)raf[lap] = Files.readAllBytes(Paths.get(configAutoKeyFolderPath + CONFIG_AUTO_KEY_FOLDER_NAME + lap + CONFIG_AUTO_KEY_FILE_EXTENSION));
 		System.out.println(fi.getPath() + "を復号中。しばらくお待ち下さい……。");
 		while((bufferLimit = bis.read(buffer)) != -1)
 		{
-			for(int lap = 0; lap < bufferLimit; lap++)
+			int sectorFront = 0;
+			int sectorRear = 0;
+			eis[] eisThreadCore = new eis[16];
+			for(int lap = 0; lap < eisThreadCore.length; lap++)
 			{
-				if(hashCounter == hashLength)
+				sectorRear += (CONFIG_BUFFER_SIZE / eisThreadCore.length);
+				eisThreadCore[lap] = new eis(true, raf, configManualKeyFrontHash, configManualKeyRearHash, configHashLength, Arrays.copyOfRange(buffer, sectorFront, sectorRear));
+				eisThreadCore[lap].start();
+				sectorFront += (CONFIG_BUFFER_SIZE / eisThreadCore.length);
+				
+				for(int blockChain = 0; blockChain < (CONFIG_BUFFER_SIZE / eisThreadCore.length) / configHashLength; blockChain++)
 				{
 					configManualKeyFrontHash = getHash(configManualKeyFrontHash);
 					configManualKeyRearHash = getHash(configManualKeyRearHash);
-					hashCounter = 0;
 				}
-				
-				
-				raf[(configManualKeyFrontHash[hashCounter] + 128)].seek((configManualKeyRearHash[hashCounter] + 128));
-				raf[(configManualKeyFrontHash[hashCounter] + 128)].read(cell);
-				
-				buffer[lap] += cell[0];
-				hashCounter++;
+			}
+			
+			buffer = null;
+			for(int lap = 0; lap < eisThreadCore.length; lap++)
+			{
+				eisThreadCore[lap].join();
+				buffer = getArray(buffer, eisThreadCore[lap].getBuffer());
+				eisThreadCore[lap] = null;
 			}
 			
 			bos.write(buffer, 0, bufferLimit);
@@ -310,7 +386,6 @@ public class eis
 			displayProgress();
 		}
 		
-		for(int lap = 0; lap < CONFIG_AUTO_KEY_SIZE; lap++)raf[lap].close();
 		bis.close();
 		bos.close();
 		System.out.print("\r");
